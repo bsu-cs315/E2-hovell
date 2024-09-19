fc7fd957674caf086f69884ac276d357727219df
Camden Hovell <camden.hovell@bsu.edu>
2024-09-18 20:27:43 -0400

    Refactor input system for ice cream

--- project/ice_cream/ice_cream.gd
+++ project/ice_cream/ice_cream.gd
@@ -4,19 +4,25 @@ extends CharacterBody2D
 const _SPEED : float = 300.0
 const _JUMP_VELOCITY : float = -775.0
 var can_move : bool = true
+var _direction : int
 
 func _physics_process(delta: float) -> void:
 	if can_move:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
 
-		if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		if Input.is_key_pressed(KEY_UP) and is_on_floor():
 			velocity.y = _JUMP_VELOCITY
 
-		var direction := Input.get_axis("ui_left", "ui_right")
+		if Input.is_key_pressed(KEY_LEFT):
+			_direction = -1
+		elif Input.is_key_pressed(KEY_RIGHT):
+			_direction = 1
+		else:
+			_direction = 0
 		
-		if direction:
-			velocity.x = direction * _SPEED
+		if _direction:
+			velocity.x = _direction * _SPEED
 		else:
 			velocity.x = move_toward(velocity.x, 0, _SPEED)
 
